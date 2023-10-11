527f1601ee908ed64cffe91647205233fe77198c
f-andrei <andreifrancap@gmail.com>
2023-10-11 15:30:43 -0300

    ignore

--- .idea/learning/generate_pw.py
+++
@@ -1,9 +0,0 @@
-import secrets
-import string as s
-from secrets import SystemRandom as Sr
-
-print(''.join(Sr().choices(s.ascii_letters + s.digits + s.punctuation, k=32)))
-random = secrets.SystemRandom()
-
-
-
