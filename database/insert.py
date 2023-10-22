0e5c0cd7082fd6d55e3f9ad547b04df18ad7d941
f-andrei <andreifrancap@gmail.com>
2023-10-22 19:49:04 -0300

    saves parent, child and task info to db

--- database/insert.py
+++ database/insert.py
@@ -1,22 +1,32 @@
 import sqlite3
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent.parent
+DB_DIR = 'database'
+DB_NAME = 'tamagochi.sqlite3'
+DB_FILE = ROOT_DIR / DB_DIR / DB_NAME
 
 # Create a connection to the SQLite database
-connection = sqlite3.connect('tamagochi.sqlite3')
+connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
 
 def save_parent_to_database(parent):
-    cursor.execute("INSERT INTO parents (name, age, gender, email, password_hash) VALUES (?, ?, ?, ?, ?)",
-                   (parent.name, parent.age, parent.gender, parent.email, parent.password))
-    connection.commit()
-    parent.parent_id = cursor.lastrowid
+    try:
+        cursor.execute("INSERT INTO parents (name, age, gender, email, password_hash) VALUES (?, ?, ?, ?, ?)",
+                       (parent.name, parent.age, parent.gender, parent.email, parent.password))
+        connection.commit()
+        parent.parent_id = cursor.lastrowid
+        print(f"Parent '{parent.name}' successfully inserted into the database.")
+    except Exception as e:
+        print(f"Error inserting parent into the database: {e}")
 
-def save_child_to_database(child):
-    cursor.execute("INSERT INTO children (name, age, gender, parent_id) VALUES (?, ?, ?, ?)",
-                   (child.name, child.age, child.gender, child.parent.parent_id))
+def save_child_to_database(child, parent_id):
+    cursor.execute("INSERT INTO children (parent_id, name, age, gender) VALUES (?, ?, ?, ?)",
+                   (parent_id, child.name, child.age, child.gender))
     connection.commit()
     child.child_id = cursor.lastrowid
 
-def save_task_to_database(task):
+def save_task_to_database(task, child_id):
     cursor.execute("INSERT INTO tasks (name, period, frequency, difficulty, reward, description, child_id) VALUES (?, ?, ?, ?, ?, ?, ?)",
-                   (task.name, task.period, task.frequency, task.difficulty, task.reward, task.description, task.child.child_id))
+                   (task.name, task.period, task.frequency, task.difficulty, task.reward, task.description, task.child_id))
     connection.commit()
